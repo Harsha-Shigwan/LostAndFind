4767d158a9941e5f45b57fb109c91146dcb610ba
Harsha-Shigwan <harshashigwan595@gmail.com>
2025-05-04 21:38:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './Components/LoginPage';
+import SignupPage from './Components/SignupPage';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import HomePage from './Components/HomePage';
+import SearchLostItems from './Components/SearchLostItem';
+import ReportLostItemForm from './Components/ReportLostItemForm';
+import NotificationSystem from './Components/NotificationSystem';
 
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/searchLost" element={<SearchLostItems/>} />
+        <Route path="/report-lost-item" element={<ReportLostItemForm />} />
+        <Route path="/notify" element={<NotificationSystem />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
